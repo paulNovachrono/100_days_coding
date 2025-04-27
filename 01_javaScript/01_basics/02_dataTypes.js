aba93a55e8450e93ef5269736bec726a0121cdd1
Debajoyti Paul <learn.ishan2001@gmail.com>
2025-04-27 10:18:09 +0000

    datatype

---
+++ 01_javaScript/01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; // the code started to execute in ES6 methods.
+
+//alert(1234); // We are using node.js browser
+
+// https://developer.mozilla.org/en-US/docs/Web/API/Window/alert
+
+/*console.log(1+
+    5
+);*/ //not readable
+
+
+let name = "Debajoyti";
+let age = 23;
+let isLoggedin = false;
+
+console.table([name, age, isLoggedin]);
+
+// some data types [primitive]
+    // number => 1234
+    //bigint => for large numbers
+    //string => "abcd"
+    //Boolean => true/false
+    //null => standalone value. represent of empty value
+    //undefined => when we don't define the value of a variable
+    // symbol => use for uniqueness
+
+// object
+console.log(typeof null); //object
+console.log(typeof undefined); // undefine
