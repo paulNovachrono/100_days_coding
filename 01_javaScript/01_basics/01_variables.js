bed8798df8e76cde8ff3708965fc8b3fe29fd0ba
Debajoyti Paul <learn.ishan2001@gmail.com>
2025-04-27 09:56:02 +0000

    variables

---
+++ 01_javaScript/01_basics/01_variables.js
@@ -0,0 +1,22 @@
+const accountId = 9875;
+let accountEmail = "info.debajoyti@gmail.com";
+var accountPassword = "12345";
+accountCity = "Kolkata";
+let state;
+
+//accountId = 10;
+//console.log(accountId);
+// variable declared by const keyword can not be changed
+
+
+/*
+Prefer not to use the "var"
+because of scope issues.
+*/
+
+accountEmail = "paulnovachrono@gmail.com";
+accountPassword = 212121;
+accountCity = "Delhi";
+console.table([accountId, accountEmail, accountPassword,accountCity, state]);
+
+
