be8955857a3df82439c1f3ba5d4d52d8897875ad
Debajoyti Paul <learn.ishan2001@gmail.com>
2025-05-03 15:21:00 +0000

    while & dowhile

---
+++ 01_javaScript/05_litrations/two.js
@@ -0,0 +1,19 @@
+let i = 0
+while(i <= 10){
+//    console.log(`This is the value: ${i}`);
+    i = i + 2
+}
+
+let arr = ["D", "e", "b", "a", "j", "o", "y", "t", "i"]
+let index = 0;
+while(index < arr.length){
+    //console.log(`This is the value in the array ${arr[index]}`);
+    index ++
+}
+
+// do while loop = executes 1st condition checks later
+let a = 5;
+do{
+    console.log(`This is the value of a:${a}`);
+    a++
+}while(a < 10)
