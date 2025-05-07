59a8fd4c38c999738e1184d231945421b5b9d163
Debajoyti Paul <learn.ishan2001@gmail.com>
2025-05-07 17:00:37 +0000

    project 2 has been done

---
+++ 01_javaScript/07_projects/02_project2/script.js
@@ -0,0 +1,33 @@
+// get the form 
+const form = document.querySelector('form')
+
+
+// we have to stop submitting default value
+form.addEventListener('submit', function(e){
+    e.preventDefault() // not submitting default value
+
+    // get the input values
+    const height = parseInt(document.querySelector('#height').value)
+    const weight = parseInt(document.querySelector('#weight').value)
+    const results = document.querySelector('#results')
+
+    // checking
+    if(height <0 || isNaN(height)){
+        console.log(height);
+        return `Not valid ${height}`
+        
+    }else if (weight < 0 || isNaN(weight)){
+        console.log(weight);
+        
+        return `Not valid ${weight}`
+    }
+    else{
+        // calculate the bmi
+        const bmi = (weight / ((height / 100) ** 2)).toFixed(2)
+
+
+        results.innerHTML = `<span>${bmi}</span>`
+        console.log(bmi);
+        
+    }
+})
